be0d8356e4093a8ebb6c6158798d0ab841915817
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-05-25 22:52:32 +0530

    Add files via upload

---
+++ #1_One/src/one/One_UserInput.java
@@ -0,0 +1,19 @@
+package one;
+import java.util.Scanner;
+
+public class One_UserInput {
+    public static void main(String[] args) {
+        System.out.println("Taking Input Form the User");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number 1");
+        int a =  sc.nextInt();
+
+        System.out.println("Enter Number 2");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("The sum is = " + sum);
+
+    }
+}
