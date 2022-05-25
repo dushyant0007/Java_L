e5a7a9bd8c1a30b48db4caea5d2a03eabe692ce0
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-05-25 22:50:37 +0530

    Add files via upload

---
+++ #1_One/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println("This is first program of May 2022");
+    }
+}
