91b7dfb6ee712c542e4864064a4af71d9458583b
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-05-27 10:07:01 +0530

    Add files via upload

---
+++ #1_One/src/one/Two_Operators.java
@@ -0,0 +1,12 @@
+package one;
+
+public class Two_Operators {
+    public static void main(String[] args) {
+
+        // Operators
+        //  + , -, *, /, ^
+        //  Logical &&, ||, >=, <=
+        // Binary &, |, ^(ExOr)
+
+    }
+}
