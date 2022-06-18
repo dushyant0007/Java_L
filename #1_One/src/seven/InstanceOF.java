bad399189197eb9334ffe079a050f54ebfecd85c
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-06-19 00:13:58 +0530

    Add files via upload

---
+++ #1_One/src/seven/InstanceOF.java
@@ -0,0 +1,12 @@
+package seven;
+
+public class InstanceOF {
+    public static void main(String[] args) {
+////        The instanceof operator in Java is used to check whether an object is an instance of a particular class or not.
+//        Its syntax is
+//        objectName instanceOf className;
+        String name = "Hero";
+        boolean result1 = name instanceof String;
+        System.out.println("name is an instance of String: " + result1);
+    }
+}
